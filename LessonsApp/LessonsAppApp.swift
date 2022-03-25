1e899f2474888aa19c92413f2a39dfdd759a499c
Kevinwtierney <kevinwtierney@gmail.com>
2022-03-25 16:38:47 -0700

    Initial Commit

---
+++ LessonsApp/LessonsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LessonsAppApp.swift
+//  LessonsApp
+//
+//  Created by Kevin Tierney on 3/25/22.
+//
+
+import SwiftUI
+
+@main
+struct LessonsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

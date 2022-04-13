e498fadde8d36121d6378e240db8a566b6b6b19b
Kevinwtierney <kevinwtierney@gmail.com>
2022-04-13 11:43:20 -0700

    - add submit button in textview -highlight appropriate colors for correct and wrong answers

--- LessonsApp/Views/TestView/TestView.swift
+++ LessonsApp/Views/TestView/TestView.swift
@@ -10,33 +10,124 @@ import SwiftUI
 struct TestView: View {
     
     @EnvironmentObject var model: ContentModel
+    @State var selectedAnswerIndex:Int?
+    @State var numCorrect = 0
+    @State var submited = false
     
     var body: some View {
         
         
-            if model.currentQuestion != nil{
+        if model.currentQuestion != nil{
+            
+            VStack(alignment:.leading){
                 
-                VStack{
+                // Question Number
+                Text("Question \(model.currentQuestionIndex + 1) of \(model.currentModule?.test.questions.count ?? 0)")
+                    .padding(.leading, 20)
+                
+                // Question
+                
+                CodeTextView()
+                    .padding(.horizontal, 20)
+                // Answer
+                ScrollView{
                     
-                    // Question Number
-                    Text("Question \(model.currentQuestionIndex + 1) of \(model.currentModule?.test.questions.count ?? 0)")
-                    
-                    // Question
-                   
-                    CodeTextView()
-                    // Answer
-                    
-                    // Button
+                    VStack{
+                        
+                        ForEach(0..<model.currentQuestion!.answers.count, id: \.self) { index in
+                            Button{
+                                // track the selected index
+                                selectedAnswerIndex = index
+                            }
+                        label: {
+                            ZStack{
+                                if submited == false{
+                                    RectangleCard(color: index == selectedAnswerIndex ? .gray : .white)
+                                        .frame(height:48)
+                                }
+                                
+                                else{
+                                    // Answer has been submitted
+                                    
+                                    if index == selectedAnswerIndex && index == model.currentQuestion!.correctIndex{
+                                        
+                                        // User has selected the right answer
+                                        // Show a green Background
+                                        RectangleCard(color: .green)
+                                            .frame(height:48)
+                                        
+                                    }
+                                    
+                                    else if index == selectedAnswerIndex && index != model.currentQuestion!.correctIndex {
+                                        
+                                        // User has selected the wrong answer
+                                        // Show a red Background
+                                        RectangleCard(color: .red)
+                                            .frame(height:48)
+                                    }
+                                    
+                                    else if index == model.currentQuestion!.correctIndex{
+                                        
+                                        // This button is the correct answer
+                                        // Show a green Background
+                                        RectangleCard(color: .green)
+                                            .frame(height:48)
+                                    }
+                                    else{
+                                        
+                                        RectangleCard(color: .white)
+                                            .frame(height:48)
+                                    }
+                                }
+                                
+                                Text(model.currentQuestion!.answers[index])
+                                
+                            }
+                            
+                        }
+                        .disabled(submited)
+                            
+                            
+                        }
+                    }
+                    .accentColor(.black)
+                    .padding()
                 }
-                .navigationBarTitle("\(model.currentModule?.category ?? "") Test")
                 
+                // Button
+                
+                Button{
+                    
+                    // change submitted state to true
+                    submited = true
+                    //submit answer and check if correct
+                    if selectedAnswerIndex == model.currentQuestion!.correctIndex{
+                        numCorrect+=1
+                    }
+                    
+                }label:{
+                    
+                    ZStack{
+                        RectangleCard(color: .green)
+                            .frame(height:48)
+                        Text("Submit")
+                            .bold()
+                            .foregroundColor(.white)
+                    }
+                    .padding()
+                }
+                .disabled(selectedAnswerIndex == nil)
             }
+            .navigationBarTitle("\(model.currentModule?.category ?? "") Test")
+            
+        }
+        
         else{
             
             ProgressView()
         }
         
-    
+        
     }
 }
 
